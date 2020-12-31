2363ccd94d487f78c60a522a79dc65ad816a0aac
Andrew Sanchez <a.vincent.sanchez@gmail.com>
2020-12-31 14:55:00 +0000

    added streaming

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-const NodeMediaServer = require("node-media-server");
 
 import App from "./components/App";
 import reducers from "./reducers";
